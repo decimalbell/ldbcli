d87afb3191667d203bc75f2127013b71e8c859f0
Junlong Xie <decimalbell@gmail.com>
2022-10-29 12:39:28 +0800

    feat: add ls command

--- cmd/ldbcli/main.go
+++ cmd/ldbcli/main.go
@@ -7,11 +7,15 @@ import (
 
 	"github.com/syndtr/goleveldb/leveldb"
 	"github.com/syndtr/goleveldb/leveldb/opt"
+	"github.com/syndtr/goleveldb/leveldb/util"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	var dbpath string
+	var (
+		dbpath string
+		prefix string
+	)
 
 	app := &cli.App{
 		Flags: []cli.Flag{
@@ -43,6 +47,40 @@ func main() {
 						fmt.Printf("%s %s\n", key, string(value))
 					}
 					return nil
+				},
+			},
+			{
+				Name:  "ls",
+				Usage: "ls",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:        "prefix",
+						Usage:       "prefix",
+						Destination: &prefix,
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					opts := &opt.Options{
+						ReadOnly: true,
+					}
+					db, err := leveldb.OpenFile(dbpath, opts)
+					if err != nil {
+						return err
+					}
+
+					var slice *util.Range
+					if prefix != "" {
+						slice = util.BytesPrefix([]byte(prefix))
+					}
+
+					iter := db.NewIterator(slice, nil)
+					for iter.Next() {
+						key := iter.Key()
+						value := iter.Value()
+						fmt.Printf("%s %s\n", key, string(value))
+					}
+					iter.Release()
+					return iter.Error()
 				},
 			},
 		},
